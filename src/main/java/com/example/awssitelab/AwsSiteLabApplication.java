c90d457742a6fc88601dc2886ee577f10a8719c8
Vlad <vlad300820001@gmail.com>
2019-12-12 10:36:43 +0200

    first commit

---
+++ src/main/java/com/example/awssitelab/AwsSiteLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.awssitelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSiteLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSiteLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awssitelab/AwsSiteLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awssitelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSiteLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
