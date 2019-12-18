c08276c3ea35438841521b20a45fe51ee7852219
Vlad <vlad300820001@gmail.com>
2019-12-18 10:26:00 +0200

    adde tests

--- src/test/java/com/example/awssitelab/AwsSiteLabApplicationTests.java
+++ src/test/java/com/example/awssitelab/AwsSiteLabApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.awssitelab;
 
+import com.example.awssitelab.controller.Controller;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AwsSiteLabApplicationTests {
 
+
+    @Autowired
+    private Controller controller;
+
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 
+    @Test
+    void shouldReturnTrueWhenDayIsReturned() throws ParseException {
+        Date now = new Date();
+        SimpleDateFormat simpleDateformat = new SimpleDateFormat("EEEE"); // the day of the week spelled out completely
+        assertThat(simpleDateformat.format(now)).isEqualTo(controller.handle());
+    }
 }
